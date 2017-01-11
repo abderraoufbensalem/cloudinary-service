668c0c2fbede3564ebdb1af00ef1b9ac420972b7
abderraoufbensalem <abderraouf.bensalem@ricardo.ch>
2017-01-11 14:55:18 +0100

    env var

--- cloudinary/singleton.go
+++ cloudinary/singleton.go
@@ -13,9 +13,9 @@ type singleton struct {
 var instance *Service
 var once sync.Once
 
-var accountKey = flag.String("schemaRegistryAPI", os.Getenv("Cloudinary_Account_Key"), "The schema registry API")
-var secretKey = flag.String("schemaRegistryAPI", os.Getenv("Cloudinary_Secret_Key"), "The schema registry API")
-var cloudName = flag.String("schemaRegistryAPI", os.Getenv("Cloudinary_Cloud_Name"), "The schema registry API")
+var accountKey = flag.String("Cloudinary_Account_Key", os.Getenv("Cloudinary_Account_Key"), "The schema registry API")
+var secretKey = flag.String("Cloudinary_Secret_Key", os.Getenv("Cloudinary_Secret_Key"), "The schema registry API")
+var cloudName = flag.String("Cloudinary_Cloud_Name", os.Getenv("Cloudinary_Cloud_Name"), "The schema registry API")
 
 func GetService() *Service {
 	flag.Parse()
